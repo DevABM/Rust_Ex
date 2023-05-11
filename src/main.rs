7445ae7680e11e6b1cd8365abebaf7f3a6379676
dev.am.balamurugan <dev.am.balamurugan@gmail.com>
2023-05-11 18:40:46 -0400

    final code updated

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
 
@@ -18,11 +16,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Wrong input");
+                continue;
+            }
+        };
 
         println!("You guessed: {guess}");
-
-        // --snip--
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
